097e921165f2de902000773023c5b06b49ed0ef0
Luis Canales <kopz9999@gmail.com>
2018-03-28 04:50:56 -0700

    updated README

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,17 @@
-#!/usr/bin/env bash
-
-# update apt
+sudo echo "LANG=en_US.UTF-8" >> /etc/environment
+sudo echo "LANGUAGE=en_US.UTF-8" >> /etc/environment
+sudo echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+sudo echo "LC_CTYPE=en_US.UTF-8" >> /etc/environment
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
+sudo apt-get -y upgrade
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections 
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get -y install oracle-java8-installer
 
-# install java
-sudo apt-get install openjdk-7-jre-headless -y
+sudo apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+sudo apt-get install gnome-icon-theme-full tango-icon-theme
+sudo echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
 
-# install elasticsearch
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.deb
-sudo dpkg -i elasticsearch-1.1.1.deb
-sudo service elasticsearch start
-
-# install head
-sudo /usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head
+sudo wget -O /opt/eclipse-java-luna-SR2-linux-gtk-x86_64.tar.gz http://ftp.fau.de/eclipse/technology/epp/downloads/release/luna/SR2/eclipse-java-luna-SR2-linux-gtk-x86_64.tar.gz
+cd /opt/ && sudo tar -zxvf eclipse-java-luna-SR2-linux-gtk-x86_64.tar.gz
